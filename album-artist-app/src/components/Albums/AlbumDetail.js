87d456710c0dca3c8e40c57508a05cfbb70c3a40
edohor <edohorvat01@gmail.com>
2020-09-18 16:22:16 +0200

    edited design on album info

--- album-artist-app/src/components/Albums/AlbumDetail.js
+++ album-artist-app/src/components/Albums/AlbumDetail.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './AlbumList.css';
+import { Link } from 'react-router-dom';
 
 function AlbumDetail(props) {
     console.log("AlbumDetail props = ", props);
@@ -9,16 +10,25 @@ function AlbumDetail(props) {
         favorite = "Remove favorite";
     }
 
+    let d = new Date(props.details.releaseDate);
+    let releaseYear = d.getFullYear();
+
+    function markFavorite(albumId) {
+        console.log("Button clicked", albumId)
+    }
+
     return (
         <div className="AlbumDetail">
             <div className="imageDiv"><img src={props.details.imageUrl} className="image"/></div>
             <div className="basicInfo">
                 <div className="title">{props.details.title}</div>
-                <div className="artist">{props.details.artistId}</div>
+                <div className="artist">
+                    <Link exact to={"/artist/" + props.details.artistId} className="grayText">{props.artist}</Link>
+                </div>
             </div>
-            <div className="releaseDate">{props.details.releaseDate}</div>
+            <div className="releaseDate"><span className="grayText">Released:</span> {releaseYear}</div>
             <div className="price">{props.details.price}</div>
-            <div className="favorite">{favorite}</div>
+            <button onClick={() => markFavorite(props.details.id)} className="favoriteButton">{favorite}</button>
         </div>
     );
 }
--- album-artist-app/src/components/Albums/AlbumList.js
+++ album-artist-app/src/components/Albums/AlbumList.js
@@ -7,6 +7,7 @@ let fetchAlbums = true;
 function AlbumList() {
     
     const [albums, setAlbums] = useState([]);
+    const [artists, setArtists] = useState([]);
 
     if (fetchAlbums) {
         fetchAlbums = false;
@@ -16,26 +17,40 @@ function AlbumList() {
         .then(
             (result) => {
                 console.log("result = ", result);
-                getAllAlbums(result);
+                setAlbums(result);
+            },
+            (error) => {
+                console.log("repos error = ", error);
+            })  
+            
+        fetch('http://localhost:3004/artists')
+        .then(res => res.json())
+        .then(
+            (result) => {
+                console.log("result = ", result);
+                setArtists(result);
             },
             (error) => {
                 console.log("repos error = ", error);
             })  
     }
-
-    function getAllAlbums(result) {
-        console.log("[getAllAlbums] result = ", result);
-
-        setAlbums(result);
-
-    }
     
     console.log("[getAllAlbums] albums = ", albums);
+    console.log("[getAllAlbums] artists = ", artists);
 
     let albumDetail = [];
-    if (albums.length > 0) {
+    if (albums.length > 0 && artists.length > 0) {
         for (let i = 0; i < albums.length; i++) {
-            albumDetail.push(<AlbumDetail details={albums[i]}/>);
+            let artistName = "";
+            for (let j = 0; j < artists.length; j++) {
+                console.log("albums = ", albums[i].artistId);
+                console.log("artists = ", artists[j].id);
+                if (albums[i].artistId===artists[j].id) {
+                    console.log("MATCH", artists[j].title);
+                    artistName = artists[j].title;
+                }
+            }
+            albumDetail.push(<AlbumDetail details={albums[i]} artist={artistName}/>);
         }
     }
 
